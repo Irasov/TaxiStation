da4f86be62dc3f9741dcf0cf85f67c192f69d60b
Irasov Igor <srnan@yandex.ru>
2016-08-03 16:06:26 +0600

    context menu and appearance edit text

--- Taxi/src/com/irasov/taxi/MainActivity.java
+++ Taxi/src/com/irasov/taxi/MainActivity.java
@@ -7,6 +7,8 @@ import com.irasov.taxi.logic.Action;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -24,9 +26,13 @@ public class MainActivity extends Activity implements OnClickListener {
 	private static final String MESSAGE_CREATE = "Taxi Station Create!";
 	private static final int ID_SORT_BY_PRICE = 1;
 	private static final int ID_SORT_BY_YEAR = 2;
+	private static final int ID_EXIT = 3;
 	private static final int ID_CATEGORY = 0;
+	private static final int ID_CATEGORY_EXIT = 1;
 	private static final String TITLE_MENU_SORT_BY_PRICE ="sort by price";
 	private static final String TITLE_MENU_SORT_BY_YEAR ="sort by year";
+	private static final String MENU_EXIT ="exit";
+	private boolean visibleMenu = false;
 	
 	
 	@Override
@@ -37,6 +43,8 @@ public class MainActivity extends Activity implements OnClickListener {
 			   taxiStation = new TaxiStation("Allo");
 			   Creator creator = new Creator(taxiStation,7);
 			   creator.createCar();
+			   visibleMenu = true;
+			   showTaxiStation.setVisibility(showTaxiStation.VISIBLE);
 			   Toast.makeText(this,MESSAGE_CREATE , Toast.LENGTH_LONG).show();
 			   break;
 		}
@@ -51,13 +59,55 @@ public class MainActivity extends Activity implements OnClickListener {
 		button = (Button)findViewById(R.id.CreateTaxiStation);
 		showTaxiStation = (EditText)findViewById(R.id.showTaxiStation);
 		button.setOnClickListener(this);
+		registerForContextMenu(showTaxiStation);		
+	}
+
+	
+	
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+		switch(v.getId()){
+		case R.id.showTaxiStation:
+			menu.add(ID_CATEGORY,ID_SORT_BY_PRICE,ID_CATEGORY,TITLE_MENU_SORT_BY_PRICE);
+			menu.add(ID_CATEGORY,ID_SORT_BY_YEAR,ID_CATEGORY,TITLE_MENU_SORT_BY_YEAR);
+			break;
+		}
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		menu.add(ID_CATEGORY,ID_SORT_BY_PRICE,ID_CATEGORY,TITLE_MENU_SORT_BY_PRICE);
 		menu.add(ID_CATEGORY,ID_SORT_BY_YEAR,ID_CATEGORY,TITLE_MENU_SORT_BY_YEAR);
+		menu.add(ID_CATEGORY_EXIT,ID_EXIT,ID_CATEGORY,MENU_EXIT);
 		return super.onCreateOptionsMenu(menu);
+	}
+	
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		menu.setGroupVisible(0, visibleMenu);
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		switch(item.getItemId()){
+	    case ID_SORT_BY_PRICE:
+	    	showTaxiStation.setText(null);
+			Action.sortCar(taxiStation.getTaxiStation(), Action.CONST_SORT_PRICE);
+			showTaxiStation.append(taxiStation.toString());
+			break;
+	    case ID_SORT_BY_YEAR:
+	    	showTaxiStation.setText(null);
+			Action.sortCar(taxiStation.getTaxiStation(), Action.CONST_SORT_YEAR);
+			showTaxiStation.append(taxiStation.toString());
+			break;
+	    case ID_EXIT:
+	    	System.exit(0);
+			break;
+	
+	}
+		return super.onContextItemSelected(item);
 	}
 
 	@Override
@@ -72,6 +122,9 @@ public class MainActivity extends Activity implements OnClickListener {
 		    	showTaxiStation.setText(null);
 				Action.sortCar(taxiStation.getTaxiStation(), Action.CONST_SORT_YEAR);
 				showTaxiStation.append(taxiStation.toString());
+				break;
+		    case ID_EXIT:
+		    	System.exit(0);
 				break;
 		}
 		return super.onOptionsItemSelected(item);
