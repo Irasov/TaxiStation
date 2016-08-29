14b76a9fe9ca522d82fff227a286033e0cf1e679
Irasov Igor <srnan@yandex.ru>
2016-08-29 15:00:58 +0600

    two actitvity

--- Taxi/src/com/irasov/taxi/MainActivity.java
+++ Taxi/src/com/irasov/taxi/MainActivity.java
@@ -5,6 +5,7 @@ import com.irasov.taxi.entity.TaxiStation;
 import com.irasov.taxi.logic.Action;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.ContextMenu;
@@ -32,9 +33,11 @@ public class MainActivity extends Activity implements OnClickListener {
 	private static final int ID_SORT_BY_YEAR = 2;
 	private static final int ID_EXIT = 3;
 	private static final int ID_ANIM = 4;
+	private static final int ID_TWO_ACTIVITY = 5;
 	private static final int ID_CATEGORY = 0;
 	private static final int ID_CATEGORY_EXIT = 1;
 	private static final String TITLE_MENU_SORT_BY_PRICE ="sort by price";
+	private static final String TITLE_MENU_TWO_ACTIVITY ="Two Activity";
 	private static final String TITLE_MENU_SORT_BY_YEAR ="sort by year";
 	private static final String TITLE_MENU_ANIM ="animation";
 	private static final String MENU_EXIT ="exit";
@@ -89,6 +92,7 @@ public class MainActivity extends Activity implements OnClickListener {
 		menu.add(ID_CATEGORY,ID_SORT_BY_PRICE,ID_CATEGORY,TITLE_MENU_SORT_BY_PRICE);
 		menu.add(ID_CATEGORY,ID_SORT_BY_YEAR,ID_CATEGORY,TITLE_MENU_SORT_BY_YEAR);
 		menu.add(ID_CATEGORY_EXIT,ID_EXIT,ID_CATEGORY,MENU_EXIT);
+		menu.add(ID_CATEGORY_EXIT,ID_TWO_ACTIVITY,ID_CATEGORY,TITLE_MENU_TWO_ACTIVITY);
 		return super.onCreateOptionsMenu(menu);
 	}
 	
@@ -140,6 +144,10 @@ public class MainActivity extends Activity implements OnClickListener {
 				break;
 		    case ID_EXIT:
 		    	System.exit(0);
+				break;
+		    case ID_TWO_ACTIVITY:
+		    	Intent intent = new Intent(this, MainActivityTwo.class);
+		    	startActivity(intent);
 				break;
 		}
 		return super.onOptionsItemSelected(item);
---
+++ Taxi/src/com/irasov/taxi/MainActivityTwo.java
@@ -0,0 +1,14 @@
+package com.irasov.taxi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivityTwo extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.two);
+	}
+
+}
